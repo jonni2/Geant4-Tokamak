8b5db7a214c8341d351f72c5faef2314381fd460
jonni2 <jonnijonni200@gmail.com>
2024-04-23 23:01:42 +0200

    Added files for Sensitive Detector (SD)

---
+++ include/Detector.hh
@@ -0,0 +1,10 @@
+#ifndef DETECTOR_HH
+#define DETECTOR_HH
+
+// This file is used to create the SENSITIVE DETECTORS that can SCORE HITS
+// This file is used to COUNT the number of neutron captures by Li6 that occur (i.e. the number of tritium atoms generated, according to: n+Li6->He4+T3)
+
+// https://www.youtube.com/watch?v=5KEB-ZrWvQ0&list=PLLybgCU6QCGWgzNYOV0SKen9vqg4KXeVL&index=7
+
+
+#endif
