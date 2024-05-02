4a0831c56275ba09000ede2ce93f902c054afcc9
jonni2 <jonnijonni200@gmail.com>
2024-05-02 23:00:26 +0200

    Cleaned useless things

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -8,17 +8,10 @@ MyPrimaryGeneratorAction::MyPrimaryGeneratorAction() {
     
     G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
     
-    // Proton
-    G4ParticleDefinition* particle = particleTable->FindParticle("proton");
-    // G4ParticleDefinition* particle = particleTable->FindParticle("geantino");
-    
-    
-    // Kinematics
-    G4ThreeVector pos(0.*m, 0.*m, 0.*m); // Position where particle is generated
-    G4ThreeVector mom(0., 1., 0.); // Momentum direction
-    
-    // fParticleGun->SetParticleDefinition(particle);
+    // Set characteristics of the generated particles
     fParticleGun->SetParticleDefinition(G4Neutron::Definition());
+    
+    // The following characteristics are set in the setup.mac file
     // fParticleGun->SetParticlePosition(pos);
     // fParticleGun->SetParticleMomentumDirection(mom);
     // fParticleGun->SetParticleMomentum(0*eV);
@@ -32,18 +25,6 @@ MyPrimaryGeneratorAction::~MyPrimaryGeneratorAction() {
 
 // Here we can invoke the generation also of particles with RANDOMIZED Energy, momentum... (which by default are NOT randomized)
 void MyPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
-    
-    if(isIon == true) {
-        // Requires that the particle is a G4Geantino
-        // Heavy ion
-        G4int Z = 6;
-        G4int A = 14;
-        G4double charge = Z*eplus;
-        
-        G4ParticleDefinition* ion = G4IonTable::GetIonTable()->GetIon(Z, A, 0*keV);
-        fParticleGun->SetParticleDefinition(ion);
-        fParticleGun->SetParticleCharge(charge);
-    }
     
     fParticleGun->GeneratePrimaryVertex(anEvent);
 }
