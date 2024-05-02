c9ce11444b892745209f34101afdb1d3856ce649
jonni2 <jonnijonni200@gmail.com>
2024-05-02 21:59:37 +0200

    main file with or without graphics

--- main.cc
+++ main.cc
@@ -25,22 +25,35 @@ int main(int argc, char** argv) {
     // Initialize Geant4 kernel i.e. construct detector, create physics processes...
     runManager->Initialize();
     
-    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+    G4UIExecutive* ui = 0;
+    
+    if(argc == 1) {
+        ui = new G4UIExecutive(argc, argv);
+    }
     
     G4VisManager* visManager = new G4VisExecutive();
     visManager->Initialize();
     
     // Get the pointer to the UI manager, which is a class automatically created when the G4RunManager is created
     G4UImanager* UImanager = G4UImanager::GetUIpointer();
-    // Execute the vis.mac macro
-    UImanager->ApplyCommand("/control/execute vis.mac");
     
-    // Execute a user-defined macro .mac
-    G4String command = "/control/execute ";
-    G4String filename = argv[1];
-    UImanager->ApplyCommand(command+filename);
+    // Execute setup.mac macro: setup neutron source
+    UImanager->ApplyCommand("/control/execute setup.mac");
     
-    ui->SessionStart();
+    // Graphics enabled
+    if(ui) {
+        // Execute the vis.mac macro: setup visualization
+        UImanager->ApplyCommand("/control/execute vis.mac");
+        ui->SessionStart();
+    
+    } else {
+        // Graphics disabled
+        // Execute a user-defined macro .mac
+        G4String command = "/control/execute ";
+        G4String filename = argv[1];
+        UImanager->ApplyCommand(command+filename);
+    }
+    
     
     delete runManager;
     delete visManager;
