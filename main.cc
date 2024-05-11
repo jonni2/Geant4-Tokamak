4e14d12da267fc1c27db8de6c08969431a30e0c7
jonni2 <jonnijonni200@gmail.com>
2024-05-12 00:28:25 +0200

    Added interactive start commands

--- main.cc
+++ main.cc
@@ -27,7 +27,15 @@ int main(int argc, char** argv) {
     
     G4UIExecutive* ui = 0;
     
-    if(argc == 1) {
+    // If one executes without arguments (only ./a.out) the graphics is enabled
+    
+    std::cout << "\n\nEnable graphics for the simulation? (yes/no)\n";
+    
+    std::string ans;
+    
+    std::cin >> ans;
+    
+    if(ans == "yes" || ans == "y") {
         ui = new G4UIExecutive(argc, argv);
     }
     
@@ -40,18 +48,35 @@ int main(int argc, char** argv) {
     // Execute setup.mac macro: setup neutron source
     UImanager->ApplyCommand("/control/execute setup.mac");
     
-    // Graphics enabled
     if(ui) {
+        // Graphics enabled
+        
         // Execute the vis.mac macro: setup visualization
         UImanager->ApplyCommand("/control/execute vis.mac");
         ui->SessionStart();
     
     } else {
         // Graphics disabled
-        // Execute a user-defined macro .mac
-        G4String command = "/control/execute ";
-        G4String filename = argv[1];
-        UImanager->ApplyCommand(command+filename);
+        
+        std::cout << "\n\nGraphics disabled. Do you want to execute the external run.mac macro? (yes/no)\n";
+        
+        std::cin >> ans;
+        
+        if(ans == "yes" || ans == "y") {
+            
+            // Run external run.mac macro
+            UImanager->ApplyCommand("/control/execute run.mac");
+            
+        } else {
+            std::cout << "The simulation will generate N neutrons inside the tokamak's vessel and evaluate their interaction with the breeding blanket to produce tritium. The simulation will output the Tritium Breeding Ratio (TBR).\nInert the number of neutrons to generate:\n";
+            
+            G4int N;
+            
+            std::cin >> N;
+            
+            G4String command = "/run/beamOn ";
+            UImanager->ApplyCommand(command+std::to_string(N));
+        }
     }
     
     
